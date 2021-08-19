b7803fd0308eaf8a971c0e0f98094614396c77f3
Luise Freese <luise@raeuberleiterin.de>
2021-08-19 19:59:12 +0200

    adds assignments values and variables as well as data types

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,60 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+// learn to use console.log
+console.log("Jonas");
+console.log(23);
+// declare a variable
+let firstName = "Matilda";
+// pass in a variable into the console.log
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+//convention camelCase notation, ALLCAPS only for constants, no special characters other than _ and $, be descriptive
+let myFirstJob = "Programmer";
+console.log(myFirstJob);
+
+// assignment values and variables
+let country = "Germany";
+let continent = "Europe";
+let population = 82000000;
+console.log(country);
+console.log(continent);
+console.log(population);
+//primitives:
+//number decimal and integers
+//string text '' or ""
+//boolean true or false
+//undefined means empty value
+//null means empty value as well
+//BigInt (for numbers to big than for Number )
+//important: value have types, not variables
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof "Luise");
+console.log(typeof 23);
+console.log(typeof javascriptIsFun);
+// dynamic typing: reassign a value to a variable
+javascriptIsFun = "yes";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1991;
+console.log(typeof year);
+console.log(year);
+// null is not an object
+console.log(typeof null);
+
+//assignment Data types
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
