78b44f48940403374fb1b62154925ef4ac2e6508
Luise Freese <luise@raeuberleiterin.de>
2021-08-20 14:34:46 +0200

    completes variables, basic operators, operator precendence

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,4 @@
-let js = "amazing";
+/*let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 // learn to use console.log
 console.log("Jonas");
@@ -47,7 +47,7 @@ console.log(typeof year);
 year = 1991;
 console.log(typeof year);
 console.log(year);
-// null is not an object
+// null is not an object 💡
 console.log(typeof null);
 
 //assignment Data types
@@ -58,3 +58,66 @@ console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+let age = 30;
+//reassign the variable
+age = 31;
+console.log(age);
+//use const if your variable won't change in the future
+
+const birthYear = 1991;
+//birthYear = 1990; // leads to uncaught TypeError, assignment to constant variable
+// const can't be mutated/reassigned
+//const can't be empty, because you need an initializer
+//use const, unless you know that the value of the variable will change in the future
+// avoid var NEVER EVER USE IT
+var job = "programmer";
+job = "teacher";
+
+//even works without let or const - but don't!
+lastName = "Freese";
+console.log(lastName);
+
+
+//operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+// you can log more than one value with console.jog()
+console.log(ageJonas, ageSarah);
+//log several operations
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 **3 means 2 to the power of 3 + 2*2*2 = 8
+console.log(ageSarah + ageJonas);
+//concatenate with plus
+const firstName = "Luise";
+const lastName = "Freese";
+console.log(firstName + " " + lastName);
+// = is an operator by itself: first it will calculate 10+6, then it wil assign the result to the variable x
+let x = 10 + 6;
+console.log(x);
+x += 10; // x = x +10 =26
+x *= 4; // x + x *4 = 104
+x++; // x + x+1
+x--; // x-1
+x--; // x-1
+console.log(x);
+//comparison operator
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+const isFullAge = ageSarah >= 18;
+//math first, then comparison
+console.log(now - 1991 > now - 2018);
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+*/
