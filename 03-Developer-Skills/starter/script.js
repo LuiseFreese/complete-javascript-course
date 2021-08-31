cf634ebc590510773ba3210b3b12f5e6e4e19a33
Luise Freese <luise@raeuberleiterin.de>
2021-08-31 16:44:46 +0200

    adds 50-56

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 23;
+
+console.log();
+
+const calcAge = birthyear => 2037 - birthyear;
+
+console.log(calcAge(1991));
+console.log(calcAge(1978));
+console.log(calcAge(1990));
