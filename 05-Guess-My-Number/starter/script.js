20187e3157de33fbac4b5fed63f02bb8dd17244d
Luise Freese <luise@raeuberleiterin.de>
2021-09-05 15:33:59 +0200

    adds 68 to 73

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🥳 Correct number!';
+document.querySelector('.score').textContent = 42;
+document.querySelector('.number').textContent = '?';
+document.querySelector('.highscore').textContent = 99;
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess, guess);
+  if (!guess) {
+    document.querySelector('.message').textContent =
+      '🤦‍♀️ You need to type in a number';
+  }
+});
