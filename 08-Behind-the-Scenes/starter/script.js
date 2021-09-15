c7aa6b468057788a28120ebac3d23b436dae569a
Luise Freese <luise@raeuberleiterin.de>
2021-09-15 15:09:28 +0200

    FEAT adds 87-93

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `You are ${age} years old, born in ${birthYear}`;
+    console.log(output);
+  }
+
+  printAge();
+
+  console.log(firstName);
+  return age;
+}
+const firstName = 'Jonas';
+calcAge(1991);
