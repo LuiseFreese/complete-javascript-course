c7351d5a4401533ce564d074649066eb1e83ee3b
Luise Freese <luise@raeuberleiterin.de>
2021-09-17 20:19:09 +0200

    FEAT adds 95

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -15,3 +15,31 @@ function calcAge(birthYear) {
 }
 const firstName = 'Jonas';
 calcAge(1991);
+
+
+// console.log(me);
+// console.log(job);
+console.log(year);
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+console.log(addDecl(2, 3));
+function addDecl(a, b) {
+  return a + b;
+}
+console.log(addExpr(2, 3));
+const addExpr = function (a, b) {
+  return a + b;
+};
+console.log(addArrow(2, 3));
+const addArrow = (a, b) => a + b;
+*/
+// delete all shoppingcarts that are empty
+
+console.log(NumProducts);
+if (!NumProducts) deleteShoppingCart();
+var numProduct = 10;
+
+function deleteShoppingCart() {
+  console.log('All items deleted');
+}
